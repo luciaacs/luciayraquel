25beaf78c6cf1b9f9a5f4138949662e9022dbdfd
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-27 18:19:50 +0100

    Update parte2.py

--- parte2/parte2.py
+++ parte2/parte2.py
@@ -7,9 +7,20 @@ import os
 os.environ['GROUP_NUMBER'] = '34'
 
 #Modificamos requirement.txt
-fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'w')
-fin.write("urllib3\n chardet\n gevent\n greenlet")
+fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'r')
+with fin as file:
+	nueva=file.read()
 fin.close()
+
+fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'w')
+with fin as file:
+	nueva=nueva.replace('urllib3==1.26.5', 'urllib3')
+	nueva=nueva.replace('chardet==3.0.4', 'chardet')
+	nueva=nueva.replace('gevent==1.4.0', 'gevent')
+	nueva=nueva.replace('greenlet==0.4.15', 'greenlet')
+	fin.write(nueva)
+fin.close()
+
 #Instalamos requirements.txt
 os.chdir('practica_creativa2/bookinfo/src/productpage') #cd
 call(['pip3', 'install', '-r', 'requirements.txt'])
