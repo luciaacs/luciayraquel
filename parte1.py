c270b8b959c1471d49d563edcd8b3f1c3c1c906f
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-27 11:36:50 +0100

    Create parte1.py

--- parte1.py
+++ parte1.py
@@ -12,10 +12,6 @@ call(['sudo', 'apt-get', 'install', 'git'])
 call(['sudo', 'apt-get', 'install', 'python3-pip'])
 call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git']) 
 
-
-#Instalamos PIP
-#call(['sudo', 'apt-get', 'install', '-y', 'python3-pip'])
-
 #Variable del entorno group number
 os.environ['GROUP_NUMBER'] = '34'
 
@@ -34,37 +30,14 @@ fin.close()
 os.chdir('practica_creativa2/bookinfo/src/productpage') #cd
 call(['pip3', 'install', '-r', 'requirements.txt'])
 
-#Modificamos productpage_monolith.py
-#call(['mv', '-f', 'productpage_monolith.py', 'in.py'])
-#fin = open('in.py', 'r')
-#fout = open('productpage_monolith.py', 'w')
-#for line in fin:
-	# Creamos variable igual que flood_factor
-#	if 'flood_factor = 0 if (os.environ.get("FLOOD_FACTOR") is None) else int(os.environ.get("FLOOD_FACTOR"))' in line :
-#		fout.write(line)
-#		fout.write('numeroGrupo = 0 if (os.environ.get("GROUP_NUMBER") is None) else int(os.environ.get("GROUP_NUMBER"))'+ os.linesep)
-#	elif "product = getProduct(product_id)" in line :
-#		fout.write(line)
-#		fout.write('    grupo = numeroGrupo') #definimos var
-#		fout.write(os.linesep)
-#	elif "\'productpage.html\'," in line :
-#		fout.write(line)
-#		fout.write('	grupo=grupo,')
-#		fout.write(os.linesep)
-#	else :
-#		fout.write(line)
-#fin.close()
-#fout.close()
-#call(['rm', '-f', 'in.py'])
 
-
-#Cambiamos el titulo de la aplicación por el nombre del grupo (en productpage.html)
+#Titulo de la aplicación es el nombre del grupo (productpage.html)
 call(['mv', '-f', 'templates/productpage.html', 'templates/in.html'])
 fin = open('templates/in.html', 'r')
 fout = open('templates/productpage.html', 'w')
 for line in fin:
 	if "{% block title %}Simple Bookstore App{% endblock %}" in line :
-		fout.write('{% block title %}GRUPO: {{ group }}{% endblock %}')
+		fout.write('{% block title %}GRUPO: {{ GROUP_NUMBER }}{% endblock %}')
 	else:
 		fout.write(line)
 fin.close()
@@ -72,8 +45,7 @@ fout.close()
 call(['rm', '-f', 'templates/in.html'])
 
 
-#Esta es solo la llamada al puerto correspondiente
-call(['python3', 'productpage_monolith.py', '9080'])
+#Ejecutamos la aplicación en el puerto correspondiente
+call(['python3', 'productpage_monolith.py', '8080'])
 
-#sudo rm/rmdir -rf 
 
