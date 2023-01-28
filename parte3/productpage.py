4011b111aee520bd226160f7f33eb4339a427aa2
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-28 11:40:49 +0100

    Create productpage.py

---
+++ parte3/productpage.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+from subprocess import call
+import sys
+import os
+import subprocess
+
+#Variable del entorno group number
+#os.environ['GROUP_NUMBER'] = '34'
+
+#Modificamos requirement.txt
+fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'r')
+with fin as file:
+	nueva=file.read()
+fin.close()
+
+fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'w')
+with fin as file:
+	nueva=nueva.replace('urllib3==1.26.5', 'urllib3')
+	nueva=nueva.replace('chardet==3.0.4', 'chardet')
+	nueva=nueva.replace('gevent==1.4.0', 'gevent')
+	nueva=nueva.replace('greenlet==0.4.15', 'greenlet')
+	fin.write(nueva)
+fin.close()
+
+#Instalamos requirements.txt
+os.chdir('practica_creativa2/bookinfo/src/productpage') #cd
+call(['pip3', 'install', '-r', 'requirements.txt'])
+
+#Titulo de la aplicación es el nombre del grupo (productpage.html)
+grupo=os.environ['GROUP_NUMBER']
+call(['mv', '-f', 'templates/productpage.html', 'templates/in.html'])
+fin = open('templates/in.html', 'r')
+fout = open('templates/productpage.html', 'w')
+for line in fin:
+	if "{% block title %}Simple Bookstore App{% endblock %}" in line :
+		fout.write("{% block title %}GRUPO: "+ grupo + "{% endblock %}")
+	else:
+		fout.write(line)
+fin.close()
+fout.close()
+call(['rm', '-f', 'templates/in.html'])
