e2a7d14182fbd2c1e82b2825dfe4554a0d06879b
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-28 13:32:00 +0100

    Create parte3.py

--- parte3/parte3.py
+++ parte3/parte3.py
@@ -4,6 +4,12 @@ import sys
 import os
 import subprocess
 
-call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
+os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 
-
+#creamos contenedror de reviews
+os.chdir('./practica_creativa2/bookinfo/src/reviews')
+call(['sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build'], shell=True)
+#creamos el resto de contenedores
+os.chdir(os.path.expanduser("~"))
+os.system('sudo docker-compose build')
+os.system('sudo docker-compose up')
