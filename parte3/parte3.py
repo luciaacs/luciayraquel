43c52b38e754791f11859427659197e152c60a91
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-28 13:43:37 +0100

    Update parte3.py

--- parte3/parte3.py
+++ parte3/parte3.py
@@ -8,6 +8,7 @@ os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 
 #creamos contenedror de reviews
 os.chdir('./practica_creativa2/bookinfo/src/reviews')
+#Necesidades
 call(['sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build'], shell=True)
 #creamos el resto de contenedores
 os.chdir(os.path.expanduser("~"))
