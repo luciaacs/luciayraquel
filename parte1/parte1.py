1c69b03c482e19f6e36ea82c1430ad913f0e2436
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-30 16:05:31 +0100

    Update parte1.py

--- parte1/parte1.py
+++ parte1/parte1.py
@@ -22,8 +22,23 @@ os.environ['GROUP_NUMBER'] = '34'
 #with fin as file:
 #	nuevo=file.read()
 #fin.close()
+#fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'w')
+#fin.write("urllib3\n chardet\n gevent\n greenlet")
+#fin.close()
+
+#Modificamos requirement.txt
+fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'r')
+with fin as file:
+	nueva=file.read()
+fin.close()
+
 fin = open('./practica_creativa2/bookinfo/src/productpage/requirements.txt', 'w')
-fin.write("urllib3\n chardet\n gevent\n greenlet")
+with fin as file:
+	nueva=nueva.replace('urllib3==1.26.5', 'urllib3')
+	nueva=nueva.replace('chardet==3.0.4', 'chardet')
+	nueva=nueva.replace('gevent==1.4.0', 'gevent')
+	nueva=nueva.replace('greenlet==0.4.15', 'greenlet')
+	fin.write(nueva)
 fin.close()
 
 #Instalamos requirements.txt
