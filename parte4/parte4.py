6aa06c4699074f62764abc9875750425523fa79c
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-01-31 12:20:24 +0100

    Update parte4.py

--- parte4/parte4.py
+++ parte4/parte4.py
@@ -3,3 +3,19 @@ from subprocess import call
 import sys
 import os
 import subprocess
+
+os.system("sudo apt-get update")
+os.systme("sudo apt-get install kubectl")
+
+os.system("sudo gcloud container clusters create cluster4 --num-nodes=5 --zone=us-central1-a")
+
+os.system("sudo kubectl apply -f details.yaml")
+os.system("sudo kubectl apply -f productpage.yaml")
+os.system("sudo kubectl apply -f ratings.yaml")
+os.system("sudo kubectl apply -f reviews-svc.yaml")
+
+os.system("sudo kubectl apply -f review-v1-deployment.yaml")
+os.system("sudo kubectl apply -f review-v2-deployment.yaml")
+os.system("sudo kubectl apply -f review-v3-deployment.yaml")
+
+os.system('sudo kubectl expose deployment productpage --type=LoadBalancer --port=9080')
