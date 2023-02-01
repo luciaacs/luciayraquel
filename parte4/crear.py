b11679a2be8466cee9e08b83e5a090ade74f52ed
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-02-01 02:09:55 +0100

    Update crear.py

--- parte4/crear.py
+++ parte4/crear.py
@@ -4,10 +4,10 @@ import sys
 import os
 import subprocess
 
-#os.system("sudo apt-get update")
-#os.system("sudo apt-get install kubectl")
+#os.system("apt-get update")
+#os.system("apt-get install kubectl")
 
-os.system("sudo gcloud container clusters create cluster --num-nodes=5 --zone=europe-southwest1-b")
+os.system("gcloud container clusters create cluster --num-nodes=5 --zone=europe-southwest1-b")
 
 
 
