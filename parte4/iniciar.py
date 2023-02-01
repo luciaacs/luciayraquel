f96f8a057fdfb3fbd3862832498c34aa6f8da747
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-02-01 01:55:13 +0100

    Update iniciar.py

--- parte4/iniciar.py
+++ parte4/iniciar.py
@@ -3,11 +3,11 @@ import sys
 import os
 import subprocess
 
-os.system("sudo kubectl apply -f details.yaml")
-os.system("sudo kubectl apply -f productpage.yaml")
-os.system("sudo kubectl apply -f ratings.yaml")
-os.system("sudo kubectl apply -f reviews-svc.yaml")
+os.system("kubectl apply -f details.yaml")
+os.system("kubectl apply -f productpage.yaml")
+os.system("kubectl apply -f ratings.yaml")
+os.system("kubectl apply -f reviews-svc.yaml")
 
-os.system("sudo kubectl apply -f review-v1-deployment.yaml")
-#os.system("sudo kubectl apply -f review-v2-deployment.yaml")
-#os.system("sudo kubectl apply -f review-v3-deployment.yaml")
+os.system("kubectl apply -f reviews-v1-deployment.yaml")
+#os.system("kubectl apply -f reviews-v2-deployment.yaml")
+#os.system("kubectl apply -f reviews-v3-deployment.yaml")
