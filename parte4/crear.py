a580d601e99b24928b930fb1d1afd3a695eb009a
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-02-01 01:43:27 +0100

    Update and rename parte4.py to crear.py

---
+++ parte4/crear.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+from subprocess import call
+import sys
+import os
+import subprocess
+
+os.system("sudo apt-get update")
+os.systme("sudo apt-get install kubectl")
+
+os.system("sudo gcloud container clusters create cluster --num-nodes=5 --zone=europe-southwest1-b")
+
+
+
+
